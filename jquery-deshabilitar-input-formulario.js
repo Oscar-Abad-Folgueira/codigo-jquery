3af4941951dec0d23786737e2da77f1f066e6e5d
Oscar Abad Folgueira <oscarabadfolgueira@gmail.com>
2022-03-16 09:19:20 +0100

    jQuery Snippet: Deshabilitar campo input de formulario por ID

---
+++ jquery-deshabilitar-input-formulario.js
@@ -0,0 +1,7 @@
+/**
+ * @snippet       jQuery Snippet: Deshabilitar campo input de formulario por ID
+ * @author        Oscar Abad Folgueira
+ * @author_url    https://www.oscarabadfolgueira.com
+ */
+
+ jQuery("#id-elemento").prop('disabled', true);
